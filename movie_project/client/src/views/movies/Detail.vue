47b720fabe2110b53e1ddc00b12cfe7326d8f93a
sang-rak <sangrack114@naver.com>
2021-05-20 10:32:59 +0900

    0520

---
+++ movie_project/client/src/views/movies/Detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="detail">
+    <h1>Movie Detail</h1>
+    <!-- <div>{{this.$store.state.catList }}</div> -->
+    {{ movie }}
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Detail',
+  props: {
+    movie: {
+      type: Array,
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
